e64fe01717bd13dc5bf8cda7db406becedf78069
ismaelpaul <ismaafp@gmail.com>
2022-10-17 14:34:13 +0100

    Add my books

--- navigation/components/MyBooks.js
+++ navigation/components/MyBooks.js
@@ -1,17 +1,48 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
+import Header from '../components/Header';
+import { firebase } from '../../config';
 
 const MyBooks = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}
-            >
-                My Books
-            </Text>
-        </View>
-    );
+	const [myBookList, setMyBookList] = useState([]);
+	const db = firebase.firestore();
+	const booksRef = db.collection('books');
+	const auth = firebase.auth();
+
+	useEffect(() => {
+		booksRef.onSnapshot((snapshot) => {
+			const books = [];
+			snapshot.forEach((doc) => {
+				const { title, author, user_id, cover_img } = doc.data();
+				if (auth.currentUser?.uid === user_id) {
+					books.push({
+						id: doc.id,
+						title,
+						author,
+						user_id,
+						cover_img,
+					});
+				}
+			});
+			setMyBookList(books);
+		});
+	}, []);
+
+	console.log(myBookList, '<<< mybook');
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<ScrollView>
+				{myBookList.map((book) => {
+					return (
+						<View key={book.id}>
+							<Text>{book.title}</Text>
+							<Image source={book.cover_img} />
+						</View>
+					);
+				})}
+			</ScrollView>
+		</View>
+	);
 };
 
 export default MyBooks;
