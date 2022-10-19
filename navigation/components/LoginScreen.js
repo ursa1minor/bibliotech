daa48c5253859adae33fad6ee015a3787cc8ab03
ismaelpaul <ismaafp@gmail.com>
2022-10-19 16:05:04 +0100

    Update login styling

--- navigation/components/LoginScreen.js
+++ navigation/components/LoginScreen.js
@@ -1,124 +1,146 @@
-import { useNavigation } from '@react-navigation/core'
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native'
-import { firebase } from '../../config'
+import { useNavigation } from '@react-navigation/core';
+import React, { useEffect, useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+	SafeAreaView,
+} from 'react-native';
+import { firebase } from '../../config';
 
 const LoginScreen = () => {
-    const auth = firebase.auth()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+	const auth = firebase.auth();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const navigation = useNavigation()
+	const navigation = useNavigation();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if (user) {
-                navigation.replace("MainContainer")
-            }
-        })
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			if (user) {
+				navigation.replace('MainContainer');
+			}
+		});
 
-        return unsubscribe
-    }, [])
+		return unsubscribe;
+	}, []);
 
-    const handleSignUp = () => {
-        navigation.replace("RegistrationForm")
-    }
+	const handleSignUp = () => {
+		navigation.replace('RegistrationForm');
+	};
 
-    const handleLogin = () => {
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user;
-            })
-            .catch(error => alert(error.message))
-    }
+	const handleLogin = () => {
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then((userCredentials) => {
+				const user = userCredentials.user;
+			})
+			.catch((error) => alert(error.message));
+	};
 
-    return (
-        <View
-            style={styles.container}
-            behavior="padding"
-        >
-            <View >
-                <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    style={styles.input}
-                    secureTextEntry
-                />
-            </View>
+	return (
+		<View style={styles.container} behavior="padding">
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputText}>Email</Text>
+				<TextInput
+					placeholder="Email"
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+					style={styles.input}
+				/>
+				<Text style={styles.inputText}>Password</Text>
+				<TextInput
+					placeholder="Password"
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+					style={styles.input}
+					secureTextEntry
+				/>
+			</View>
 
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity onPress={handleLogin} style={styles.button}>
+					<Text style={styles.buttonText}>Login</Text>
+				</TouchableOpacity>
 
-export default LoginScreen
+				<View style={styles.registerWrapper}>
+					<Text style={styles.text}>
+						Don't have an account yet?
+						<TouchableOpacity style={styles.signUp} onPress={handleSignUp}>
+							{' '}
+							Sign Up{' '}
+						</TouchableOpacity>
+					</Text>
+				</View>
+			</View>
+		</View>
+	);
+};
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '100%'
-    },
-    input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-
-
-    },
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	inputContainer: {
+		width: '90%',
+	},
+	input: {
+		width: '100%',
+		padding: 15,
+		backgroundColor: 'white',
+		marginTop: 5,
+		borderRadius: '.5rem',
+		color: '#979797',
+	},
+	buttonContainer: {
+		width: '90%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 40,
+	},
+	button: {
+		backgroundColor: '#0782F9',
+		width: '100%',
+		padding: 15,
+		borderRadius: '.5rem',
+		alignItems: 'center',
+	},
+	buttonOutline: {
+		backgroundColor: 'white',
+		marginTop: 5,
+		borderColor: '#0782F9',
+		borderWidth: 2,
+	},
+	buttonText: {
+		color: 'white',
+		fontWeight: '700',
+		fontSize: 16,
+	},
+	buttonOutlineText: {
+		color: '#0782F9',
+		fontWeight: '700',
+		fontSize: 16,
+	},
+	inputText: {
+		fontSize: 14,
+		fontWeight: 'bold',
+		marginTop: 15,
+	},
+	text: {
+		marginTop: 20,
+	},
+	registerWrapper: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	signUp: {
+		fontWeight: 'bold',
+		paddingLeft: '.2rem',
+	},
+});
