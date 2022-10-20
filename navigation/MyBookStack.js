4d01645a14658f7856224c8d58a0504e69f660d8
ismaelpaul <ismaafp@gmail.com>
2022-10-20 10:51:11 +0100

    Fixed message

---
+++ navigation/MyBookStack.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import BookCard from './components/BookCard';
+import Home from './components/HomeScreen';
+import MyBooks from './components/MyBooks';
+
+const bookCard = 'Book Card';
+const home = 'Home';
+const myBooks = 'My Books';
+const Stack = createNativeStackNavigator();
+
+export const MyBooksStack = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				options={{ headerShown: false }}
+				name={myBooks}
+				component={MyBooks}
+			/>
+			<Stack.Screen
+				options={{ headerShown: false }}
+				name={bookCard}
+				component={BookCard}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+export default MyBooksStack;
